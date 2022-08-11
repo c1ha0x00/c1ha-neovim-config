7bbdb7de6d088a11dd656c461ac7e43ebc12f79b
c1ha0x00 <c1ha0x00@126.com>
2022-08-11 17:34:00 +0800

    treesitter增量 缩进 代码折叠

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -20,4 +20,25 @@ treesitter.setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  -- 启用增量选择模块
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      node_decremental = "<BS>",
+      scope_incremental = "<TAB>",
+    },
+  },
+  -- 启用代码缩进模块 (=)
+  indent = {
+    enable = true,
+  },
 })
+
+-- 开启 Folding 模块
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- 默认不要折叠
+-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.opt.foldlevel = 99
