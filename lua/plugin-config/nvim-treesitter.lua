97d79ffbcf6e9049ce33ed1ae62e1c6e930a9be9
c1ha0x00 <c1ha0x00@126.com>
2022-08-11 17:19:07 +0800

    format nvim-tressitter.lua

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -7,7 +7,14 @@ end
 treesitter.setup({
   -- 安裝 language parser
   -- :TSInstallInfo 命令查看支持的語言
-  ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx"},
+  ensure_installed = { "json", 
+                    "html",
+                    "css",
+                    "vim",
+                    "lua",
+                    "javascript", 
+                    "typescript", 
+                    "tsx"},
   -- 启用代码高亮模块
   highlight = {
     enable = true,
