2030b0547cd00d343396eefa1b908f2ce8bacda0
c1ha0x00 <c1ha0x00@126.com>
2022-11-23 11:07:55 +0800

    rust-tools

--- lua/plugin-config/lsp/mason.lua
+++ lua/plugin-config/lsp/mason.lua
@@ -63,9 +63,24 @@ mason_lspconfig.setup_handlers({
     }
   end,
   -- Next, you can provide targeted overrides for specific servers.
-  --["rust_analyzer"] = function ()
-  -- require("rust-tools").setup {}
-  --end,
+
+  -- rust
+  ["rust_analyzer"] = function()
+    local rt = require("rust-tools")
+    rt.setup({
+      server = {
+        on_attach = function(client, bufnr)
+          -- Hover actions
+          vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+          -- Code action groups
+          vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+          require('keybindings').map_lsp_on_attach(client, bufnr)
+        end,
+      },
+    })
+  end,
+
+  -- lua
   ["sumneko_lua"] = function()
     lspconfig.sumneko_lua.setup {
       on_attach = on_attach,
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,39 +1,45 @@
 local packer = require("packer")
 packer.startup(
   function(use)
-  -- Packer 可以管理自己本身
-  use('wbthomason/packer.nvim')
-  ----------- colorschemes ----------
-  -- tender
-  use("jacoborus/tender.vim")
-  -- tokyonight
-  use("folke/tokyonight.nvim")
-  -----------------------------------
-  ----------- plugins ---------------
-  -- nvim-tree
-  use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons"})
-  -- bufferline
-  use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye"}})
-  -- lualine
-  use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
-  use("arkav/lualine-lsp-progress")
-  -- treesitter
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-  -- LSP & lspconfig
-  use {
+    -- Packer 可以管理自己本身
+    use('wbthomason/packer.nvim')
+    ----------- colorschemes ----------
+    -- tender
+    use("jacoborus/tender.vim")
+    -- tokyonight
+    use("folke/tokyonight.nvim")
+    -----------------------------------
+    ----------- plugins ---------------
+    -- nvim-tree
+    use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+    -- bufferline
+    use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" } })
+    -- lualine
+    use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
+    use("arkav/lualine-lsp-progress")
+    -- treesitter
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    -- LSP & lspconfig
+    use {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "neovim/nvim-lspconfig",
-  }
-  -- telescope (fzf tool)
-  use { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } }
-  -- dashboard
-  use("glepnir/dashboard-nvim")
-  -- project
-  use("ahmedkhalf/project.nvim")
+      -- rust-tools
+      use 'simrat39/rust-tools.nvim',
 
-  -- cmp
-  use {
+      -- Debugging
+      use 'nvim-lua/plenary.nvim',
+      use 'mfussenegger/nvim-dap',
+    }
+    -- telescope (fzf tool)
+    use { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } }
+    -- dashboard
+    use("glepnir/dashboard-nvim")
+    -- project
+    use("ahmedkhalf/project.nvim")
+
+    -- cmp
+    use {
       -- cmp
       'hrsh7th/nvim-cmp',
       -- snippet engine
@@ -45,11 +51,11 @@ packer.startup(
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
       'amarakon/nvim-cmp-buffer-lines',
-      -- snippets 
+      -- snippets
       "rafamadriz/friendly-snippets",
     }
-  -----------------------------------
-end
+    -----------------------------------
+  end
 )
 
 -- 每次保存plugins.lua 自动安装插件
